6e1af78084dd8d806257ba3eb98f7599713c7326
mirakwon24 <118599870+mirakwon24@users.noreply.github.com>
2022-12-22 16:42:47 +0400

    Update function.py

--- function.py
+++ function.py
@@ -1,44 +1,27 @@
-documents = [
-        {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-        {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-        {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
-        {"type": "driver license", "number": "5455 028765", "name": "Василий Иванов"},
-      ]
-
-
-directories = {
-        '1': ['2207 876234', '11-2', '5455 028765'],
-        '2': ['10006'],
-        '3': []
-      }
-
-def get_name():
-    number = input('Введите номер документа ')
-    for data in documents:
-        if data.get("number") == number:
-            return data.get('name')
-    return 'Документа с таким номером нет'
+def get_name(docs, number):
+    documents = list(filter(lambda x: x['number'] == number, docs))
+    if documents:
+        return documents[0]['name']
+    else:
+        return 'Документа с таким номером нет'
  
-def get_shelf():
-    number = input('Введите номер документа ')
-    for key in directories :
-        if number in directories.get(key):
+ 
+def get_shelf(dirs, number):
+    for key, value in dirs.items():
+        if number in value:
             return key
     return 'В полках документа с данным номером нет.'
  
-def get_list(docs):
-    for doc in docs:
+ 
+def get_list():
+    for doc in documents:
         return f"{doc['type']} {doc['number']} {doc['name']};"
  
  
-def add_doc():
-    shelf = input('Введите номер полки куда положить документ. ')
-    if shelf not in directories:
-        return 'Нет такой полки'
-    doc = {}
-    for info in ('type','number', 'name'):
-        doc[info] = input(f'{info}: ')
-    directories[shelf] = directories.get(shelf).append(doc['number'])
+def add_doc(docs, shelfs, shelf, type, number, name):
+    doc = {'type': type, 'number': number, 'name': name}
+    docs.append(doc)
+    shelfs[shelf].append(doc['number'])
     return 'Документ добавлен'
  
  
@@ -59,13 +42,22 @@ while True:
     comand = input('Введите название команды ')
  
     if comand == 'p':
-        print(get_name())
+        num = input('Введите номер документа ')
+        print(get_name(documents, num))
     
     elif comand == 's':
-        print(get_shelf())
+        num = input('Введите номер документа ')
+        print(get_shelf(directories, num))
     
     elif comand == 'l':
-        print(get_list())
+        get_list(documents)
     
     elif comand == 'a':
-            print(add_doc())
+        shelf = input('Введите номер полки куда положить документ. ')
+        if shelf in directories.keys():
+            type = input('Введите тип документа. ')
+            number = input('Введите номер документа. ')
+            name = input('Введите имя владельца документа ')        
+            add_doc(documents, directories, shelf, type, number, name)
+        else:
+            print("Номер полки введен не верно")
